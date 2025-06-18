d11b4d3c455297140069d48276b8eccd3f123ab4
ljkkj7 <kianjenkins26@gmail.com>
2025-06-18 20:24:49 +0100

    Bug fix - Refactored Stock django model to have 10 digits rather than 5

--- backend/marketio_backend/stockhandler/models.py
+++ backend/marketio_backend/stockhandler/models.py
@@ -10,8 +10,8 @@ from django.utils import timezone
 class Stock(models.Model):
     symbol = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    base_price = models.DecimalField(max_digits=5, decimal_places=2, default=100.00)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2, default=100.00)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def simulate_price_change(self):
