be1b0660f079455187c7bf7bdb6b75ad5088e71b
ljkkj7 <kianjenkins26@gmail.com>
2025-06-26 12:14:56 +0100

    Change reference to custom_auth in dashboard django app to prevent manage.py throwing a migration error

--- backend/marketio_backend/dashboard/views.py
+++ backend/marketio_backend/dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from ..custom_auth.models import UserProfile
-from ..custom_auth.serializers import UserProfileSerializer
+from custom_auth.models import UserProfile
+from custom_auth.serializers import UserProfileSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
