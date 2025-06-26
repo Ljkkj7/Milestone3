fcd7fdb8c6641fe18a6eccd240be4f2c4b22dbad
ljkkj7 <kianjenkins26@gmail.com>
2025-06-26 12:50:55 +0100

    Added JWT checking when fetching user balance data

--- backend/marketio_backend/dashboard/views.py
+++ backend/marketio_backend/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from custom_auth.models import UserProfile
 from custom_auth.serializers import UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -11,6 +12,9 @@ class BalanceLoadView(APIView):
     """
     A view to load a users balance value
     """
+
+    permission_classes = [IsAuthenticated]
+
     def get(self, req):
         balance = UserProfile.objects.get(user=req.user)
         serializer = UserProfileSerializer(balance)
