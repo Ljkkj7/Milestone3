3040556c50b060ecb53f66cfc48fb1ff761d3d4c
ljkkj7 <kianjenkins26@gmail.com>
2025-06-26 14:36:15 +0100

    Add - Url patterns for buy stock view

--- backend/marketio_backend/stockhandler/urls.py
+++ backend/marketio_backend/stockhandler/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import StockListAPIView, StockUpdateAPIView
+from .views import StockListAPIView, StockUpdateAPIView, BuyStockView
 
 
 urlpatterns = [
     path('', StockListAPIView.as_view(), name='stock-list'),
     path('update/', StockUpdateAPIView.as_view(), name='stock-update'),
+    path('buy/', BuyStockView.as_view(), name='stock-buy')
 ]
