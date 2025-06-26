a73316f382c690ae830d91f9b781b6cdca2a6cee
ljkkj7 <kianjenkins26@gmail.com>
2025-06-26 15:38:43 +0100

    Add - implemented sell stock view

--- backend/marketio_backend/stockhandler/urls.py
+++ backend/marketio_backend/stockhandler/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import StockListAPIView, StockUpdateAPIView, BuyStockView
+from .views import StockListAPIView, StockUpdateAPIView, BuyStockView, SellStockView
 
 
 urlpatterns = [
     path('', StockListAPIView.as_view(), name='stock-list'),
     path('update/', StockUpdateAPIView.as_view(), name='stock-update'),
-    path('buy/', BuyStockView.as_view(), name='stock-buy')
+    path('buy/', BuyStockView.as_view(), name='stock-buy'),
+    path('sell/', SellStockView.as_view(), name='stock-sell'),
 ]
--- backend/marketio_backend/stockhandler/views.py
+++ backend/marketio_backend/stockhandler/views.py
@@ -66,4 +66,45 @@ class BuyStockView(APIView):
 
 
 
-# class SellStockView(APIView):
+class SellStockView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        user_profile = UserProfile.objects.get(user=user)
+        symbol = request.data.get("symbol")
+        quantity = int(request.data.get("quantity"))
+
+        stockOwned = sum(t.quantity for t in Transaction.objects.filter(user_profile=user_profile, symbol=symbol, transaction_type='BUY')) - \
+                     sum(t.quantity for t in Transaction.objects.filter(user_profile=user_profile, symbol=symbol, transaction_type='SELL'))
+
+        if not symbol or not quantity:
+            return Response({'error': 'Symbol and quantity are requred.'})
+        
+        try:
+            if quantity <= 0:
+                return Response({'error': 'Quantity must be positive'})
+            elif quantity > stockOwned:
+                return Response({'error': 'Not enough stock to sell'})
+        except ValueError:
+            return Response({'error': 'Quantity must be an integer'})
+        
+        try:
+            stock = Stock.objects.get(symbol=symbol)
+        except Stock.DoesNotExist:
+            return Response({'error': 'Stock not found'})
+        
+        totalPrice = stock.price * quantity
+
+        user_profile.balance += totalPrice
+        user_profile.save()
+
+        Transaction.objects.create(
+            user_profile=user_profile,
+            stock=stock,
+            quantity=quantity,
+            transaction_type='SELL',
+            price=stock.price
+        )
+
+        return Response({'message': f'Successfully sold {quantity} shares of {symbol}'})
