8ad0ded671f56f4263bca1c46b51857385e2a72a
ljkkj7 <kianjenkins26@gmail.com>
2025-07-29 13:50:42 +0100

    Add method to handle get request in leaderboard view

--- backend/marketio_backend/leaderboard/views.py
+++ backend/marketio_backend/leaderboard/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from custom_auth.models import UserProfile
 from custom_auth.serializers import UserProfileSerializer
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -18,4 +19,12 @@ class LeaderboardView(APIView):
         """
         Return the top 10 users ordered by experience points.
         """
-        return UserProfile.objects.order_by('-experience')[:10]    
+        return UserProfile.objects.order_by('-experience')[:10]
+
+    def get(self, request):
+        """
+        Handle GET request to retrieve the leaderboard.
+        """
+        queryset = self.get_queryset()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)    
