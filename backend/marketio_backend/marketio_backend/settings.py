0130e82b7f1fd4f68bccb82e2bdfc07b63f6e730
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2025-12-02 13:03:20 +0000

    Update SECRET_KEY to use an empty default

--- backend/marketio_backend/marketio_backend/settings.py
+++ backend/marketio_backend/marketio_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-f0dt=1y_a0w1#mx7gjmgv0q%d3vi+i9ri5criw+%2h=(%t%f=5')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 ENVIRONMENT = os.getenv('ENV', 'development')
 
