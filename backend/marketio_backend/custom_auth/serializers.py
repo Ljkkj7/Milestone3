5a83409f3bde037c5b9f9da740232229a821db8e
ljkkj7 <kianjenkins26@gmail.com>
2026-08-19 15:04:12 +0100

    fix for registration

--- backend/marketio_backend/custom_auth/serializers.py
+++ backend/marketio_backend/custom_auth/serializers.py
@@ -7,7 +7,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'balance', 'level', 'experience']
 
     def create(self, validated_data):
         user = User.objects.create_user(
